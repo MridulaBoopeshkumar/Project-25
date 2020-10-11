7f784ba2d9efd57862bb4bacb6eb065cc1bf0416
MridulaBoopeshkumar <69774015+MridulaBoopeshkumar@users.noreply.github.com>
2020-10-11 18:05:30 -0500

    Add files via upload

---
+++ bin.js
@@ -0,0 +1,20 @@
+class Bin {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: true
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      this.image = loadImage("sprites/dustbingreen.png");
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      imageMode(CENTER);
+
+      //fill("green");
+      //stroke("green");
+      image(this.image, 500,630 ,this.width, this.height);
+    }
+  }
